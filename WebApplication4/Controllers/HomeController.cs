9c041e273fca93d8fd99e958a3f8f3687b422799
Michał Nabożny <michal.pgs@windowslive.com>
2019-01-17 19:31:27 +0100

    Add project files.

---
+++ WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static List<Lookup> lookup = new List<Lookup>() { new Lookup() { Id = 1, Key = "1", Value = "V1" },
+                                                            new Lookup() { Id = 2, Key = "2", Value = "V2" } };
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult GetIndex()
+        {
+            return Json(lookup, JsonRequestBehavior.AllowGet);
+        }
+
+
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public JsonResult Create(Lookup lookup)
+        {
+            lookup.Id = HomeController.lookup.Max(x => x.Id) + 1;
+            HomeController.lookup.Add(lookup);
+
+            return Json(lookup.Id);
+        }
+
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public JsonResult Edit(Lookup lookup)
+        {
+            HomeController.lookup.Find(x => x.Id == lookup.Id).Key = lookup.Key;
+            HomeController.lookup.Find(x => x.Id == lookup.Id).Value = lookup.Value;
+
+            return Json(lookup.Id);
+        }
+
+        [HttpPost]
+        public ActionResult Remove(Lookup lookup)
+        {
+            HomeController.lookup.RemoveAt(lookup.Id);
+
+            return new HttpStatusCodeResult(HttpStatusCode.OK);
+        }
+
+
+
+
+
+
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication4/Scripts/tutorial.js
@@ -0,0 +1,115 @@
+﻿viewModel = {
+    lookupCollection: ko.observableArray()
+};
+
+
+$(document).ready(function () {
+    $.ajax({
+        type: "GET",
+        url: "/Home/GetIndex",
+    }).done(function (data) {
+        $(data).each(function (index, element) {
+            var mappedItem =
+            {
+                Id: ko.observable(element.Id),
+                Key: ko.observable(element.Key),
+                Value: ko.observable(element.Value),
+                Mode: ko.observable("display")
+            };
+            viewModel.lookupCollection.push(mappedItem);
+        });
+        ko.applyBindings(viewModel);
+    }).error(function (ex) {
+        alert("Error");
+    });
+});
+
+
+$(document).on("click", ".kout-cancel", null, function (ev) {
+    var current = ko.dataFor(this);
+    current.Mode("display");
+    current.Key(before.Key);
+    current.Value(before.Value);
+});
+
+var before = { Id: "", Key: "", Value: "" };
+
+$(document).on("click", ".kout-edit", null, function (ev) {
+    var current = ko.dataFor(this);
+    current.Mode("edit");
+
+    before.Id = current.Id();
+    before.Key = current.Key();
+    before.Value = current.Value();
+
+});
+
+
+$(document).on("click", ".kout-save", null, function (ev) {
+    var current = ko.dataFor(this);
+    current.Mode("display");
+    saveData(current);
+});
+
+
+$(document).on("click", ".kout-delete", null, function (ev) {
+    var current = ko.dataFor(this);
+    removeData(current);
+   
+});
+
+
+
+$(document).on("click", "#create", null, function (ev) {
+    var current = {
+        Id: ko.observable(0),
+        Key: ko.observable(),
+        Value: ko.observable(),
+        Mode: ko.observable("edit")
+    };
+    viewModel.lookupCollection.push(current);
+});
+
+
+function saveData(currentData) {
+    var postUrl = "";
+    var submitData = {
+        Id: currentData.Id(),
+        Key: currentData.Key(),
+        Value: currentData.Value()
+    };
+    if (currentData.Id() && currentData.Id() > 0) {
+        postUrl = "/Home/Edit";
+    }
+    else {
+        postUrl = "/Home/Create";
+    }
+    $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        url: postUrl,
+        data: JSON.stringify(submitData)
+    }).done(function (id) {
+        currentData.Id(id);
+    }).error(function (ex) {
+        alert("ERROR Saving");
+    });
+}
+
+function removeData(currentData) {
+    var submitData = {
+        Id: currentData.Id(),
+        Key: currentData.Key(),
+        Value: currentData.Value()
+    };
+    $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        url: "/Home/Remove",
+        data: JSON.stringify(submitData)
+    }).done(function (id) {
+        viewModel.lookupCollection.remove(currentData);
+    }).error(function (ex) {
+        alert("error");
+    });
+}
